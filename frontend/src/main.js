478accc8347acce208806b0e45768e36d47abcb1
Balaj Bogdan Andrei <andrei.balaj@emailtree.ai>
2025-11-24 19:22:53 +0200

    Adding tailwindcss/vite package

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,8 @@ import { useAuthStore } from './stores/auth'
 import App from './App.vue'
 import router from './router'
 
+import './index.css' 
+
 const app = createApp(App)
 
 app.use(createPinia())
