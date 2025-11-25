879042752b9d09b79f11ecceff71d8cabb2679c2
Balaj Bogdan Andrei <andrei.balaj@emailtree.ai>
2025-11-25 20:18:44 +0200

    Backend: Fix allowed origins frontend url

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -8,7 +8,7 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',     // Vue/Vite dev server
         'http://127.0.0.1:5173',
-        'https://task-frontend-972999949636.us-central1.run.app', // Your production frontend
+        'https://frontend-972999949636.us-central1.run.app', // Your production frontend
     ],
 
     'allowed_origins_patterns' => [],
